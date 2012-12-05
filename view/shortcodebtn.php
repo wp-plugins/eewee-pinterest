3dc1b846eb79064f3e39b17863e9b88f37a2adb8
eewee <eewee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-05 19:36:00 +0000

    version 1.3 : improvement shortcode pinterest
    git-svn-id: https://plugins.svn.wordpress.org/eewee-pinterest/trunk@634501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view/shortcodebtn.php
+++
@@ -1,6 +0,0 @@
-<?php 
-if( empty($url) ){    $tbl['url']    = "http://pinterest.com/michaeldumontet/";   }
-if( empty($name) ){   $tbl['name']   = "eewee";     }
-?>
-
-<a data-pin-do="buttonFollow" href="<?php echo $url; ?>"><?php echo $name; ?></a>
